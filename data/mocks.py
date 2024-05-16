8647600d874ea32050b2fe3dead95ba8c76e5bf7
Andrea <andru8912@hotmail.com>
2024-05-15 22:29:32 -0500

    first commit

---
+++ data/mocks.py
@@ -0,0 +1,43 @@
+#******  CALIDAD DEL AIRE  *******
+#nombre
+#comuna
+#ICA
+#fecha
+#correo
+
+
+
+
+
+#******  RUIDO AMBIENTAL ********
+#comuna
+#direccion
+#nombre
+#decibeliosdiurnos
+#decibeliosnocturnos
+#fecha
+
+
+
+#****** SIEMBRA DE ARBOLES ********
+#corregimiento
+#hectareassembradas
+#especiesembrada
+#nombres
+#fecha
+#correo
+
+
+#****** CONSUMO ABUSIVO DEL AGUA ******
+#comuna
+#direccion
+#nombre
+#fecha
+#cantidadlitros
+
+#****** DEFORESTACION *****
+#corregimiento
+#hectareasperdidas
+#direccion
+#nombre
+#fecha
